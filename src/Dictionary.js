f7682f5db3b8e116610a2cd0161d550b237e46e9
Roopa S <roopa31113@gmail.com>
2024-05-30 12:19:13 +0530

    xDictionary logic implemented

---
+++ src/Dictionary.js
@@ -0,0 +1,40 @@
+import React, {useState} from "react";
+import "./Dictionary.css";
+export default function Dictionary(){
+    const dictionary=
+    [
+        { word: "React", meaning: "A JavaScript library for building user interfaces." },
+        { word: "Component", meaning: "A reusable building block in React." },
+        { word: "State", meaning: "An object that stores data for a component." }
+    ]
+    
+    const [input, setInput] = useState("");
+    const [def, setDef] = useState("");
+
+    const handleSearch = () => {
+        const foundWord = dictionary.find((e)=>e.word.toLowerCase() === input.toLowerCase());
+
+        if(foundWord){
+            setDef(foundWord.meaning);
+        }
+        else{
+            setDef("Words not found in the dictionary.");
+        }
+    }
+
+    return(
+        <div>
+            <h1>Dictionary App</h1>
+            <input 
+                placeholder="Search for a word" 
+                name="input" 
+                value={input} 
+                onChange={(e)=>setInput(e.target.value)}
+            />
+            <button onClick={handleSearch}>Search</button>
+            <h4>Definition:</h4>
+            <p>{def}</p>
+        </div>
+        
+    );
+}
